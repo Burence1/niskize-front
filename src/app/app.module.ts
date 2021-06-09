0e0d46165ff317277be8dfed926014c10e41b181
Burence1 <burensomondi@gmail.com>
2021-06-09 14:59:17 +0300

    add required folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticlesComponent } from './components/articles/articles/articles.component';
+import { PostsComponent } from './components/posts/posts/posts.component';
+import { UsersComponent } from './components/users/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticlesComponent,
+    PostsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
