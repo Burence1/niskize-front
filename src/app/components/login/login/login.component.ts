64af3c2e12a1af19b9a39c00f3f74671b2268cd5
Burence1 <burensomondi@gmail.com>
2021-06-09 23:50:50 +0300

    add login

--- src/app/components/login/login/login.component.ts
+++ src/app/components/login/login/login.component.ts
@@ -1,4 +1,8 @@
+import { AuthService } from './../../../services/auth/auth.service';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginform: FormGroup;
+  isAuthenticated: boolean = false;
 
-  constructor() { }
+  constructor(private loginservice: AuthService, private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.loginform = new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  get form() {
+    return this.loginform.controls;
+  }
+
+  onSubmit() {
+    const data = {
+      username: this.form.username.value,
+      password: this.form.password.value,
+    };
+    this.loginservice.login(data).subscribe(
+      (response) => {
+        // this.toastr.success('Login successfull');
+        this.router.navigate(['home']);
+        this.isAuthenticated = true;
+        console.log(response);
+      },
+      (error) => {
+        alert('Login unsuccessfull');
+      }
+    );
+
+
   }
 
 }
